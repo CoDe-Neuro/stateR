b259cde97a80bcaf9cf9b4ee5239623946e84851
lucasfr <lucas.franca@northumbria.ac.uk>
2026-05-31 15:01:15 +0100

    🔧 chore: fix R CMD CHECK warnings — add VignetteBuilder, NAMESPACE imports, globalVariables, and .Rbuildignore entries

---
+++ R/utils.R
@@ -0,0 +1,11 @@
+#' @importFrom dplyr %>% group_by ungroup arrange mutate summarise filter select
+#'   rename lag all_of
+#' @importFrom tidyr nest unnest unnest_wider
+#' @importFrom purrr map
+#' @importFrom janitor tabyl
+#' @importFrom glue glue
+#' @importFrom stats lag
+utils::globalVariables(c(
+  "clus", "clusVec", "cluster", "dwell", "fo",
+  "n", "tag", "target", "tot", ".x[[i]]"
+))
